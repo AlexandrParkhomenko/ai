16b215336e78a89284d9924f12096b1c6477e3e2
AlexandrParkhomenko <48244777+AlexandrParkhomenko@users.noreply.github.com>
2020-11-18 15:10:36 +0300

    gradient contribution

---
+++ MITx 6.036 1T2019/week5_exercises_2_2_gradient_contribution.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 18 12:38:56 2020
+@author:  Alexandr Parkhomenko 
+Ex2.2: What is the gradient contribution from each point to the parameters of the blue (lower) line
+
+Hint: Please do not make assumptions more difficult than necessary.
+"""
+
+import numpy as np
+
+θ0 = 0
+θ = np.array([[1]])
+x = np.array([[1,1,3,3]])
+y = np.array([[3,1,2,6]])
+
+for i in range(4):
+    z = np.array([ -2*(y[:,i] -np.dot(θ.T,x[:,i]) -θ0)*x[:,i], -2*(y[:,i] -np.dot(θ.T,x[:,i]) -θ0)])
+    print("z:",z.T)
